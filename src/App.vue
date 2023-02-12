dac935e87c239a01e6224d67dd3a310bf4154055
Jaynob <jynobpapiya12@gmail.com>
2023-02-12 11:17:45 +0600

    first-commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <form-helper>
+      <h2 slot="title">I am the slot title</h2>
+      <p slot="text">I am the paragraph text for slot</p>
+    </form-helper>
+  </div>
+
+
+</template>
+
+
+
+<script>
+import formHelper from './components/formHelper.vue';
+
+export default {
+  components:{
+    'form-helper': formHelper
+    
+  },
+
+  data () {
+    return {
+      title: 'I am a dynamic slot title'
+        
+    
+    }
+  },
+  methods:{
+    
+  }
+
+  
+}
+</script>
+
+<style >
+
+</style>
